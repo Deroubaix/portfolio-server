c64fd8e1f29d6bf9a1b5ffd7385ef03a869e8884
Marisha Deroubaix <deroubaix.marisha@gmail.com>
2023-06-14 16:33:57 +0100

    add existing project files

--- server.js
+++ server.js
@@ -1,27 +1,33 @@
-require('dotenv').config(); 
+require('dotenv').config();
 const express = require('express');
 const nodemailer = require('nodemailer');
 const { google } = require('googleapis');
 const cors = require('cors');
 
+const PORT = process.env.PORT || 5005;
+
 const app = express();
-const port = 5005; 
-app.use(cors({ origin: 'http://localhost:5173' }));
+
+// Add CSP middleware
+app.use((req, res, next) => {
+  res.setHeader('Content-Security-Policy', "default-src 'none'; font-src 'self' data:;");
+  next();
+});
 
 
+
+app.use(cors({ origin: ['http://localhost:5173'] }));
 app.use(express.json());
-
 
 const oAuth2Client = new google.auth.OAuth2(
   process.env.EMAIL_CLIENT_ID,
   process.env.EMAIL_CLIENT_SECRET,
-  'https://developers.google.com/oauthplayground' 
+  'https://developers.google.com/oauthplayground'
 );
 
 oAuth2Client.setCredentials({
   refresh_token: process.env.EMAIL_REFRESH_TOKEN,
 });
-
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
@@ -35,20 +41,17 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-
 app.post('/api/send-email', async (req, res) => {
   const { name, email, message } = req.body;
 
   try {
-   
     const mailOptions = {
-      from: email, 
+      from: email,
       to: process.env.EMAIL_USER,
       subject: 'New contact form submission',
       text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
     };
 
-   
     await transporter.sendMail(mailOptions);
     console.log('Email sent successfully');
     res.status(200).send('Email sent successfully');
@@ -58,9 +61,8 @@ app.post('/api/send-email', async (req, res) => {
   }
 });
 
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
 });
 
 
